4516f7816d7fe746c481639b3a2a0f812d046b23
Sumedha Kodithuwakku <sumedhassk@gmail.com>
2015-08-31 21:42:16 +0530

    Updating copyright

--- cloud-backends/components/user-store/org.wso2.carbon.cloud.userstore/src/main/java/org/wso2/carbon/cloud/userstore/config/CloudRealmConfigBuilder.java
+++ cloud-backends/components/user-store/org.wso2.carbon.cloud.userstore/src/main/java/org/wso2/carbon/cloud/userstore/config/CloudRealmConfigBuilder.java
@@ -15,6 +15,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
+
 package org.wso2.carbon.cloud.userstore.config;
 
 import org.apache.commons.logging.Log;
--- cloud-backends/components/user-store/org.wso2.carbon.cloud.userstore/src/main/java/org/wso2/carbon/cloud/userstore/tenant/CloudTenantManager.java
+++ cloud-backends/components/user-store/org.wso2.carbon.cloud.userstore/src/main/java/org/wso2/carbon/cloud/userstore/tenant/CloudTenantManager.java
@@ -15,6 +15,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
+
 package org.wso2.carbon.cloud.userstore.tenant;
 
 import org.apache.axiom.om.OMElement;
