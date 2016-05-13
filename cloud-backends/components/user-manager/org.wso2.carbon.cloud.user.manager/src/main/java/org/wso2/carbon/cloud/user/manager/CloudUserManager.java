82a518988cad3cf4eb4c94e14c769b6977b5fd0f
Sumedha Kodithuwakku <sumedhassk@gmail.com>
2016-05-13 11:55:26 +0530

    Refactoring CloudUserManager service

--- cloud-backends/components/user-manager/org.wso2.carbon.cloud.user.manager/src/main/java/org/wso2/carbon/cloud/user/manager/CloudUserManager.java
+++ cloud-backends/components/user-manager/org.wso2.carbon.cloud.user.manager/src/main/java/org/wso2/carbon/cloud/user/manager/CloudUserManager.java
@@ -1,138 +1,98 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.carbon.cloud.user.manager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.cloud.user.manager.beans.TenantInfoBean;
+import org.wso2.carbon.cloud.user.manager.util.CloudUserManagerConstants;
+import org.wso2.carbon.cloud.user.manager.util.CloudUserManagerException;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.base.MultitenantConstants;
-import org.wso2.carbon.cloud.user.manager.beans.TenantInfoBean;
-import org.wso2.carbon.cloud.user.manager.internal.ServiceReferenceHolder;
-import org.wso2.carbon.user.api.UserStoreException;
-import org.wso2.carbon.user.api.UserStoreManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 /**
  * Custom LDAP based user store implementation for cloud.
  */
 public class CloudUserManager {
 
-	private static Log log = LogFactory.getLog(CloudUserManager.class);
-	public static final String CLOUD_MGT_DATASOURCE = "cloud_mgt";
+    private static Log log = LogFactory.getLog(CloudUserManager.class);
 
-	public TenantInfoBean getTenantDisplayNames(String user)
-			throws SQLException {
-		TenantInfoBean tenantInfo = new TenantInfoBean();
-		Connection conn = null;
-		PreparedStatement preparedStatement = null;
-		try {
-			Hashtable<String, String> environment = new Hashtable<String, String>();
-			environment.put("java.naming.factory.initial",
-					"org.wso2.carbon.tomcat.jndi.CarbonJavaURLContextFactory");
-			Context initContext = new InitialContext(environment);
-			DataSource dataSource = (DataSource) initContext.lookup("jdbc/"
-					+ CLOUD_MGT_DATASOURCE);
-			if (dataSource != null) {
-				conn = dataSource.getConnection();
-				preparedStatement = conn
-						.prepareStatement("SELECT * FROM ORGANIZATIONS WHERE"
-								+ " tenantDomain IN (SELECT tenantDomain FROM TENANT_USER_MAPPING WHERE userName=?)");
-				preparedStatement.setString(1, user);
-				ResultSet rs = preparedStatement.executeQuery();
-				while (rs.next()) {
-					String tenantDomain = rs.getString("tenantDomain");
-					String tenantDisplayName = rs.getString("displayName");
-					tenantInfo.setTenantDisplayName(tenantDisplayName);
-					tenantInfo.setTenantName(tenantDomain);
-				}
-			} else {
-				log.info("Cannot Find a data source with the name : "
-						+ CLOUD_MGT_DATASOURCE);
-			}
-		} catch (NamingException e) {
-			log.error(e.getMessage());
-		} catch (SQLException e) {
-			log.error(e.getMessage());
-		} finally {
-			if (preparedStatement != null) {
-				preparedStatement.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		}
-		return tenantInfo;
-	}
-
-	public boolean checkForDefaultRole(String userName) {
-		log.info("Checking Default Role for user : " + userName);
-		if (isExistingUser(userName) && !isInDefaultRole(userName)) {
-			log.info("Default Role not assigned for user : " + userName);
-			addDefaultLoginRole(userName);
-			return true;
-		}
-		return false;
-	}
-
-	private boolean isInDefaultRole(String userName) {
-		UserStoreManager userStoreManager;
-		try {
-			userStoreManager = ServiceReferenceHolder.getRealmService()
-					.getTenantUserRealm(MultitenantConstants.SUPER_TENANT_ID)
-					.getUserStoreManager();
-			String[] rolesOfUser = userStoreManager.getRoleListOfUser(userName);
-			for (String s : rolesOfUser) {
-				if ("default".equals(s)) {
-					return true;
-				}
-			}
-		} catch (UserStoreException e) {
-			String msg = "Failed to check users default " + " failed due to "
-					+ e.getMessage();
-			log.error(msg);
-		}
-		return false;
-	}
-
-	private boolean isExistingUser(String user) {
-		UserStoreManager userStoreManager;
-		try {
-			userStoreManager = ServiceReferenceHolder.getRealmService()
-					.getTenantUserRealm(MultitenantConstants.SUPER_TENANT_ID)
-					.getUserStoreManager();
-			return userStoreManager.isExistingUser(user);
-		} catch (UserStoreException e) {
-			String msg = "Failed to check the user availability "
-					+ " failed due to " + e.getMessage();
-			log.error(msg);
-		}
-		return false;
-	}
-
-	private void addDefaultLoginRole(String user) {
-		UserStoreManager userStoreManager;
-		try {
-			userStoreManager = ServiceReferenceHolder.getRealmService()
-					.getTenantUserRealm(MultitenantConstants.SUPER_TENANT_ID)
-					.getUserStoreManager();
-
-			String[] roles = { "default" };
-			userStoreManager.updateRoleListOfUser(user, null, roles);
-			log.info("Default Role assigned for user : " + user);
-		} catch (UserStoreException e) {
-			String msg = "Error adding the default role to the user "
-					+ " failed due to " + e.getMessage();
-			log.error(msg);
-		}
-	}
+    public TenantInfoBean[] getTenantDisplayNames() throws CloudUserManagerException {
+        ArrayList<TenantInfoBean> tenantInfoList;
+        String loggedInUser = PrivilegedCarbonContext.getThreadLocalCarbonContext().getUsername();
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            tenantInfoList = new ArrayList<TenantInfoBean>();
+            Hashtable<String, String> environment = new Hashtable<String, String>();
+            environment.put("java.naming.factory.initial", "org.wso2.carbon.tomcat.jndi.CarbonJavaURLContextFactory");
+            Context initContext = new InitialContext(environment);
+            DataSource dataSource = (DataSource) initContext.lookup(CloudUserManagerConstants.CLOUD_MGT_DATASOURCE);
+            if (dataSource != null) {
+                conn = dataSource.getConnection();
+                preparedStatement = conn.prepareStatement("SELECT * FROM ORGANIZATIONS WHERE"
+                        + " tenantDomain IN (SELECT tenantDomain FROM TENANT_USER_MAPPING WHERE userName=?)");
+                preparedStatement.setString(1, loggedInUser);
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next()) {
+                    TenantInfoBean tenantInfo = new TenantInfoBean();
+                    String tenantDomain = rs.getString("tenantDomain");
+                    String tenantDisplayName = rs.getString("displayName");
+                    tenantInfo.setTenantDisplayName(tenantDisplayName);
+                    tenantInfo.setTenantName(tenantDomain);
+                    tenantInfoList.add(tenantInfo);
+                }
+            } else {
+                log.info("Cannot Find a data source with the name : " + CloudUserManagerConstants.CLOUD_MGT_DATASOURCE);
+            }
+        } catch (NamingException e) {
+            String msg =
+                    "Error occurred while getting the datasource : " + CloudUserManagerConstants.CLOUD_MGT_DATASOURCE;
+            log.error(msg, e);
+            throw new CloudUserManagerException(msg, e);
+        } catch (SQLException e) {
+            String msg = "Error occurred while getting the tenant information of user : " + loggedInUser;
+            log.error(msg, e);
+            throw new CloudUserManagerException(msg, e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                log.warn("Error occurred while cleaning up SQL connections.", e);
+            }
+        }
+        return tenantInfoList.toArray(new TenantInfoBean[tenantInfoList.size()]);
+    }
 
 }
