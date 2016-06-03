2d0722068cd897d37a6519f446073678bc4a1eb4
dilhasha <dilhasha.uom@gmail.com>
2016-06-03 11:05:49 +0530

    DAS tool added to cloudadmin

---
+++ cloud-applications/cloud-admin/src/site/themes/default/templates/dasDataPurge/js/dasDataPurge.js
@@ -0,0 +1,86 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+var submitType;
+
+function doSubmitDate() {
+    submitType = 1;
+    var year = $("#year").attr('value');
+    var month = $("#month").attr('value');
+
+    jagg.post("../blocks/dasDataPurge/ajax/dasDataPurge.jag", {
+        action:"purgeDataBasedOnDate",
+        year : year,
+        month : month
+    }, function (result) {
+        result = JSON.parse(result);
+        if (!result.error) {
+            var response = result.data;
+            if(response == true){
+                $("#dateBasedPurgeNotification").html("<span class='label label-success'>"+"Data Purge has successfully " +
+                    "completed."+"</span>");
+            } else {
+                $("#dateBasedPurgeNotification").html("<span class='label label-danger'>"+"Data Purge was not successful."
+                    +"</span>");
+            }
+        } else {
+            jagg.message({type:'error',content:'<strong>Error!</strong>' + result.message});
+        }
+
+    });
+}
+
+function doSubmitTenant() {
+    submitType = 2;
+    var tenantDomain = $("#tenantDomain").attr('value');
+
+    jagg.post("../blocks/dasDataPurge/ajax/dasDataPurge.jag", {
+        action:"purgeDataBasedOnTenant",
+        tenantDomain : tenantDomain
+    }, function (result) {
+        result = JSON.parse(result);
+        if (!result.error) {
+            var response = result.data;
+            if(response == true){
+                $("#tenantBasedPurgeNotification").html("<span class='label label-success'>"+"Data Purge has successfully " +
+                    "completed."+"</span>");
+            } else {
+                $("#tenantBasedPurgeNotification").html("<span class='label label-danger'>"+"Data Purge was not successful."
+                    +"</span>");
+            }
+        } else {
+            jagg.message({type:'error',content:'<strong>Error!</strong>' + result.message});
+        }
+    });
+}
+
+
+$(document).ready(function () {
+    $('input[type="radio"]').click(function() {
+        if ($(this).attr("value") == "two") {
+            $("#purgeDataByDate").hide();
+            $("#purgeDataByTenant").show();
+            $("#tenantBasedPurgeNotification").html("");
+        }
+        if ($(this).attr("value") == "one") {
+            $("#purgeDataByTenant").hide();
+            $("#purgeDataByDate").show();
+            $("#dateBasedPurgeNotification").html("");
+        }
+    });
+});
