a3ff906c22716739821431f31aa0db74b54699fa
Vinuri <vinurip@Vinuris-MacBook-Pro.local>
2016-10-31 15:31:07 +0530

    Fixing the payment method issue

--- cloud-backends/components/billing/org.wso2.carbon.cloud.billing.vendor/src/main/java/org/wso2/carbon/cloud/billing/vendor/stripe/StripeCloudBilling.java
+++ cloud-backends/components/billing/org.wso2.carbon.cloud.billing.vendor/src/main/java/org/wso2/carbon/cloud/billing/vendor/stripe/StripeCloudBilling.java
@@ -919,7 +919,7 @@ public class StripeCloudBilling implements CloudBillingServiceProvider {
                     invoiceItemObj.addProperty("Amount", invoiceItem.get("total").asText());
                     invoiceItemObj.addProperty("paid", invoiceItem.get("paid").asText());
                     invoiceArrayList.add(invoiceItemObj.toString());
-                    if (invoiceItem.get("charge").asBoolean()) {
+                    if (!invoiceItem.get("charge").isNull()) {
                         //get ChargeObject
                         String chargeDetails = getChargedDetails(invoiceItem.get("charge").asText());
                         JsonNode chargeObj = APICloudMonetizationUtils.getJsonList(chargeDetails);
