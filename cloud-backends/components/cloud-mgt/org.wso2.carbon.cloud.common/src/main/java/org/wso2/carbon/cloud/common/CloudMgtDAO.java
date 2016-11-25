12cb6cdce11d14d0ff78c4875325beb40e2c4ae1
Shenavi de Mel <shenavi@wso2.com>
2016-11-25 15:48:00 +0530

    Refactoring invite.jag block's function  db calls from jaggery to Java

--- cloud-backends/components/cloud-mgt/org.wso2.carbon.cloud.common/src/main/java/org/wso2/carbon/cloud/common/CloudMgtDAO.java
+++ cloud-backends/components/cloud-mgt/org.wso2.carbon.cloud.common/src/main/java/org/wso2/carbon/cloud/common/CloudMgtDAO.java
@@ -1,5 +1,7 @@
 package org.wso2.carbon.cloud.common;
 
+import org.json.simple.JSONObject;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,14 +11,16 @@ import java.sql.SQLException;
  * This class acts ass the database access class for the cloudmgt operations.
  */
 public class CloudMgtDAO {
-    private static final String selectEmailFromTempInvitee = "SELECT email FROM TEMP_INVITEE WHERE uuid=(?)";
-    private static final String selectEmailFromTempRegistration =
+    private static final String selectEmailFromTempInviteeQuery = "SELECT email FROM TEMP_INVITEE WHERE uuid=(?)";
+    private static final String selectEmailFromTempRegistrationQuery =
             "SELECT email FROM TEMP_REGISTRATION WHERE uuid=(?)";
-    private static final String SELECT_ROLES_FROM_TEMP_INVITEE =
+    private static final String selectRolesFromTempInviteeQuery =
             "SELECT roles FROM TEMP_INVITEE WHERE tenantDomain=(?) AND email=(?)";
-    private static final String INSERT_INTO_TEMP_INVITEE =
+    private static final String insertIntoTempInviteeQuery =
             "INSERT INTO TEMP_INVITEE VALUES (? , ? , ? , ?, CURRENT_TIMESTAMP, ?) ON " +
             "DUPLICATE KEY UPDATE uuid=(?), roles=(?), isSelfSigned=(?) , dateTime = CURRENT_TIMESTAMP";
+    private static final String selectUUIDAndRolesOfTempInviteeQuery =
+            "SELECT uuid, roles FROM TEMP_INVITEE WHERE email=(?) AND tenantDomain = (?)";
 
     /**
      * This method returns the emails for the self-registered and the invited users from the cloud_mgt database
@@ -38,9 +42,9 @@ public class CloudMgtDAO {
                     .getDbConnection();
             if (conn != null) {
                 if (isInvitedUser) {
-                    ps = conn.prepareStatement(selectEmailFromTempInvitee);
+                    ps = conn.prepareStatement(selectEmailFromTempInviteeQuery);
                 } else {
-                    ps = conn.prepareStatement(selectEmailFromTempRegistration);
+                    ps = conn.prepareStatement(selectEmailFromTempRegistrationQuery);
                 }
                 ps.setString(1, uuid);
                 resultSet = ps.executeQuery();
@@ -56,5 +60,136 @@ public class CloudMgtDAO {
             CloudMgtDBConnectionManager.closeAllConnections(ps, conn, resultSet);
         }
         return email;
+    }
+
+    /**
+     * This method retrieves the roles which were added for the temp entry for the invited users
+     *
+     * @param tenantDomain
+     * @param email
+     * @return the roles as a comma seperated string
+     * @throws CloudMgtException
+     */
+    public String getRolesOfTempInvitees(String tenantDomain, String email) throws CloudMgtException {
+
+        Connection conn = null;
+        ResultSet resultSet = null;
+        PreparedStatement ps = null;
+        String roles = null;
+
+        try {
+            conn = CloudMgtDBConnectionManager
+                    .getDbConnection();
+            if (conn != null) {
+                ps = conn.prepareStatement(selectRolesFromTempInviteeQuery);
+                ps.setString(1, tenantDomain);
+                ps.setString(2, email);
+                resultSet = ps.executeQuery();
+                while (resultSet.next()) {
+                    roles = resultSet.getString("roles");
+                }
+            }
+        } catch (SQLException e) {
+            throw new CloudMgtException(
+                    "Failed to retrieve the roles for the user " + email + " of the tenant domain " + tenantDomain, e);
+        } catch (CloudMgtException e) {
+            throw new CloudMgtException("Failed to get database connection for the cloudmgt database ", e);
+        } finally {
+            CloudMgtDBConnectionManager.closeAllConnections(ps, conn, resultSet);
+        }
+        return roles;
+    }
+
+    /**
+     * This adds a temp entry to the database for the invited users
+     *
+     * @param tenantDomain
+     * @param email
+     * @param uuid
+     * @param roles
+     * @param isSelfSigned
+     * @return if the insert operation was a success or not
+     * @throws CloudMgtException
+     */
+    public boolean insertIntoTempInvitee(String tenantDomain, String email, String uuid, String roles, int isSelfSigned)
+            throws CloudMgtException {
+
+        Connection conn = null;
+        ResultSet resultSet = null;
+        PreparedStatement ps = null;
+        boolean executionResult = false;
+
+        try {
+            conn = CloudMgtDBConnectionManager
+                    .getDbConnection();
+            if (conn != null) {
+                ps = conn.prepareStatement(insertIntoTempInviteeQuery);
+                ps.setString(1, tenantDomain);
+                ps.setString(2, email);
+                ps.setString(3, uuid);
+                ps.setString(4, roles);
+                ps.setInt(5, isSelfSigned);
+                ps.setString(6, uuid);
+                ps.setString(7, roles);
+                ps.setInt(8, isSelfSigned);
+                ps.executeUpdate();
+                executionResult = true;
+            }
+        } catch (SQLException e) {
+            throw new CloudMgtException(
+                    "Failed to insert the temp invitee details for the " + email + " of the tenant domain " +
+                    tenantDomain + " with the uuid " + uuid, e);
+        } catch (CloudMgtException e) {
+            throw new CloudMgtException("Failed to get database connection for the cloudmgt database ", e);
+        } finally {
+            CloudMgtDBConnectionManager.closeAllConnections(ps, conn, resultSet);
+        }
+        return executionResult;
+    }
+
+    /**
+     * This method retrieves the uuid and the roles of temp invited users
+     *
+     * @param email
+     * @param tenantDomain
+     * @return a json object containing the roles and the uuid
+     * @throws CloudMgtException
+     */
+    public JSONObject getRolesAndUUIDOfTempInvitees(String email, String tenantDomain) throws CloudMgtException {
+
+        Connection conn = null;
+        ResultSet resultSet = null;
+        PreparedStatement ps = null;
+        String roles = null;
+        String uuid = null;
+
+        JSONObject resultObj = null;
+
+        try {
+            conn = CloudMgtDBConnectionManager
+                    .getDbConnection();
+            if (conn != null) {
+                ps = conn.prepareStatement(selectUUIDAndRolesOfTempInviteeQuery);
+                ps.setString(1, email);
+                ps.setString(2, tenantDomain);
+                resultSet = ps.executeQuery();
+                while (resultSet.next()) {
+                    roles = resultSet.getString("roles");
+                    uuid = resultSet.getString("uuid");
+                }
+                resultObj = new JSONObject();
+                resultObj.put("roles", roles);
+                resultObj.put("uuid", uuid);
+            }
+        } catch (SQLException e) {
+            throw new CloudMgtException(
+                    "Failed to retrieve the roles and uuid for the user " + email + " of the tenant domain " +
+                    tenantDomain, e);
+        } catch (CloudMgtException e) {
+            throw new CloudMgtException("Failed to get database connection for the cloudmgt database ", e);
+        } finally {
+            CloudMgtDBConnectionManager.closeAllConnections(ps, conn, resultSet);
+        }
+        return resultObj;
     }
 }
