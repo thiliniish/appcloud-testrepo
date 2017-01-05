2f0a9b9b9432569615b16afda53686e5f494570f
Isuru Wimalasundera <isuruw@Isurus-MacBook-Pro.local>
2017-01-05 11:37:19 +0530

    removing an unnecessary package import

--- cloud-tools/tenant-cleanup-tools/components/org.wso2.carbon.cloud.tenantdeletion/src/main/java/org/wso2/carbon/cloud/tenantdeletion/utils/DataAccessManager.java
+++ cloud-tools/tenant-cleanup-tools/components/org.wso2.carbon.cloud.tenantdeletion/src/main/java/org/wso2/carbon/cloud/tenantdeletion/utils/DataAccessManager.java
@@ -29,7 +29,6 @@ import org.wso2.carbon.cloud.tenantdeletion.constants.DeletionQueries;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
