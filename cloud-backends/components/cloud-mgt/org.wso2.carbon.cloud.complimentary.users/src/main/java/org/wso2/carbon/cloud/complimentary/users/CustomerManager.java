982a75acb83979962effa5d1048ac2a09abd2402
SabraO <sabraosn@gmail.com>
2017-02-21 10:39:15 +0530

    Fix for #871 and #874

--- cloud-backends/components/cloud-mgt/org.wso2.carbon.cloud.complimentary.users/src/main/java/org/wso2/carbon/cloud/complimentary/users/CustomerManager.java
+++ cloud-backends/components/cloud-mgt/org.wso2.carbon.cloud.complimentary.users/src/main/java/org/wso2/carbon/cloud/complimentary/users/CustomerManager.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 /**
- * This class is where the business logic of the Customer service is implemented.
+ * This class is where the business logic of the Customer validation service is implemented.
  *
  * @since 1.0.0
  */
@@ -35,32 +35,26 @@ public class CustomerManager {
     }
 
     /**
-     * This method is used to retrieve the list of products for which the customer has production support from
-     * Salesforce.
+     * Method to retrieve the list of products for which the customer has production support from Salesforce.
      *
      * @param opportunities List of Jira keys
      * @return List of products which have production support for the given jira
-     * @throws CustomerException Thrown if the backend resources produce errors
+     * @throws SalesforceException Thrown if the backend resources produce errors
      */
-    public static List<String> getProdProductsforOpportunities(List<String> opportunities) throws CustomerException {
+    public static List<String> getProdProductsforOpportunities(List<String> opportunities) throws SalesforceException {
         if (opportunities.size() <= 0) {
             return new ArrayList<>();
         }
-        try {
-            SalesforceConnector salesforceConnector = new SalesforceConnector();
-            salesforceConnector.login(config.getUsername(), config.getPassword(), config.getToken());
-            @SuppressWarnings("unchecked")
-            List<String> salesforceProducts = salesforceConnector.getProductsByOpportunitiesIds(opportunities);
-            salesforceConnector.logout();
-            return salesforceProducts;
-        } catch (SalesforceException e) {
-            log.error(e.getMessage());
-            throw new CustomerException(e);
-        }
+        SalesforceConnector salesforceConnector = new SalesforceConnector();
+        salesforceConnector.login(config.getUsername(), config.getPassword(), config.getToken());
+        @SuppressWarnings("unchecked")
+        List<String> salesforceProducts = salesforceConnector.getProductsByOpportunitiesIds(opportunities);
+        salesforceConnector.logout();
+        return salesforceProducts;
     }
 
     /**
-     * Method to check if given customer has production support for given product
+     * Method to check if given customer has production support for given product.
      *
      * @param opportunities List of production opportunities for customer
      * @param productName   Product name
@@ -69,16 +63,28 @@ public class CustomerManager {
      */
     public static boolean hasProductionSupportForProduct(List<String> opportunities, String productName)
             throws CustomerException {
-        List<String> products = getProdProductsforOpportunities(opportunities);
-        for (int i = 0; i < products.size(); i++) {
-            String product = products.get(i);
-            if (product.contains(productName)) {
-                return true;
+        try {
+            List<String> products = getProdProductsforOpportunities(opportunities);
+            for (int i = 0; i < products.size(); i++) {
+                String product = products.get(i);
+                if (product.contains(productName)) {
+                    return true;
+                }
             }
+            return false;
+        } catch (SalesforceException e) {
+            log.error(e);
+            throw new CustomerException(e);
         }
-        return false;
     }
 
+    /**
+     * Method to check if the complimentary account been claimed.
+     *
+     * @param opportunities List of production opportunities for customer
+     * @return Whether the complimentary account has been claimed
+     * @throws CustomerException
+     */
     public static boolean hasClaimedComplimentarySubscription(List<String> opportunities) throws CustomerException {
         try {
             SalesforceConnector salesforceConnector = new SalesforceConnector();
@@ -89,11 +95,18 @@ public class CustomerManager {
             salesforceConnector.logout();
             return hasClaimedComplimentarySubscripton;
         } catch (SalesforceException e) {
-            log.error(e.getMessage());
+            log.error(e);
             throw new CustomerException(e);
         }
     }
 
+    /**
+     * Method to get who has claimed the subscription for given support account.
+     *
+     * @param opportunities List of production opportunities for customer
+     * @return Name of the contact who claimed the subscription
+     * @throws CustomerException
+     */
     public static String getContactWhoClaimedSubscription(List<String> opportunities) throws CustomerException {
         try {
             SalesforceConnector salesforceConnector = new SalesforceConnector();
@@ -103,11 +116,18 @@ public class CustomerManager {
             salesforceConnector.logout();
             return contact;
         } catch (SalesforceException e) {
-            log.error(e.getMessage());
+            log.error(e);
             throw new CustomerException(e);
         }
     }
 
+    /**
+     * Method to get production opportunities for customer given the production Jira keys
+     *
+     * @param customerProdJiraKeys List of production support Jira keys for customer
+     * @return List of production opportunities for customer
+     * @throws CustomerException
+     */
     public static List<String> getOpportunitiesByJira(String[] customerProdJiraKeys) throws CustomerException {
         try {
             SalesforceConnector salesforceConnector = new SalesforceConnector();
@@ -117,11 +137,18 @@ public class CustomerManager {
             salesforceConnector.logout();
             return opportunities;
         } catch (SalesforceException e) {
-            log.error(e.getMessage());
+            log.error(e);
             throw new CustomerException(e);
         }
     }
 
+    /**
+     * Method to update Salesforce account object indicating the complimentary account was claimed.
+     *
+     * @param opportunities List of production opportunities for customer
+     * @param update        true or false
+     * @throws CustomerException
+     */
     public static void updateSalesforceAccountObject(List<String> opportunities, boolean update)
             throws CustomerException {
         try {
@@ -131,11 +158,18 @@ public class CustomerManager {
             salesforceConnector.updateSObject("Account", accountId, update);
             salesforceConnector.logout();
         } catch (SalesforceException e) {
-            log.error(e.getMessage());
+            log.error(e);
             throw new CustomerException(e);
         }
     }
 
+    /**
+     * Method to update Salesforce contact object indicating whom claimed the complimentary account.
+     *
+     * @param contactId Id of contact who claimed the subscription
+     * @param update    true or false
+     * @throws CustomerException
+     */
     public static void updateSalesforceContactObject(String contactId, boolean update) throws CustomerException {
         try {
             SalesforceConnector salesforceConnector = new SalesforceConnector();
@@ -143,11 +177,19 @@ public class CustomerManager {
             salesforceConnector.updateSObject("Contact", contactId, update);
             salesforceConnector.logout();
         } catch (SalesforceException e) {
-            log.error(e.getMessage());
+            log.error(e);
             throw new CustomerException(e);
         }
     }
 
+    /**
+     * Method to get the contact Id from Salesforce given the email of the customer.
+     *
+     * @param opportunities List of production opportunities for customer
+     * @param email         Email of customer
+     * @return Id of customer
+     * @throws CustomerException
+     */
     public static String getContactId(List<String> opportunities, String email) throws CustomerException {
         try {
             SalesforceConnector salesforceConnector = new SalesforceConnector();
@@ -156,7 +198,7 @@ public class CustomerManager {
             salesforceConnector.logout();
             return contactId;
         } catch (SalesforceException e) {
-            log.error(e.getMessage());
+            log.error(e);
             throw new CustomerException(e);
         }
     }
---
+++ cloud-backends/components/cloud-mgt/org.wso2.carbon.cloud.complimentary.users/src/main/java/org/wso2/carbon/cloud/complimentary/users/utils/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. All Rights Reserved.
+ */
+package org.wso2.carbon.cloud.complimentary.users.utils;
+
+/**
+ * Constants.
+ *
+ * @since 1.0.0
+ */
+public class Constants {
+    public static final String OPPORTUNITY_NAME_CUSTOM_FIELD = "Opportunity_Name__c";
+    public static final String PRODUCT_CODE_FIELD = "ProductCode";
+    public static final String COMPLIMENTARY_CLOUD_SUBSCRIBER_CUSTOM_FIELD = "Complementary_Cloud_Subscriber__c";
+    public static final String ACCOUNT_ID_FIELD = "AccountId";
+    public static final String EMAIL_FIELD = "Email";
+    public static final String ID_FIELD = "Id";
+}
